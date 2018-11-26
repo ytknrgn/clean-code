4f246e09a3dc327336fcd30d788480d1efa64def
ytknrgn <42815334+ytknrgn@users.noreply.github.com>
2018-11-26 16:42:41 +0000

    updated eslint integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,15 @@ module.exports = {
     'function-paren-newline': 'off', // Incompatible with prettier
     'space-before-function-paren': 0, // Incompatible with prettier
     'implicit-arrow-linebreak': 'off',
+    'operator-linebreak': 'off',
+
     'max-len': ['error', 100, 2, { ignoreUrls: true }], // airbnb is allowing some edge cases
     'no-console': 'off', // airbnb is using warn
     'no-alert': 'error', // airbnb is using warn
     'no-nested-ternary': 'off',
     'no-param-reassign': 'off', // Not our taste?
     radix: 'off', // parseInt, parseFloat radix turned off. Not my taste.
+    'no-prototype-builtins': 'off',
 
     'react/require-default-props': 'off', // airbnb use error
     'react/forbid-prop-types': 'off', // airbnb use error
@@ -35,19 +38,12 @@ module.exports = {
     'react/no-unused-prop-types': 'off', // Is still buggy
     'react/jsx-one-expression-per-line': 'off',
     'react/prefer-stateless-function': 'off',
-    
+    'react/destructuring-assignment': 'off',
+
     'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'jsx-a11y/label-has-for': [
-      2,
-      {
-        required: {
-          every: ['id'],
-        },
-      },
-    ], // for nested label htmlFor error
-
-    'prettier/prettier': ['error'],
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
   },
 };
